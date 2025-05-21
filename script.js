3b04cb17e57ecdcff0ab4031ae1a694836862214
Praveenkanth G <praveen2005.21@gmail.com>
2025-05-21 21:49:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
-function result{
-var bd = document.getElenentById("bd").value;
-var bm = document.getElenentById("bm").value;
-var by = document.getElenentById("by").value;
 
-var date = new Date();
-var cd = date.getDate();
-var cm = date.getMonth();
-var cy= date.getFullYear();
+var bd=document.getElementById("date");
+var bm=document.getElementById("month");
+var by=document.getElementById("year");
 
-if (bd>cd){
-    d = cd+30;
-    m = cm-1;
-    
+var today=new Date();
+
+var cd=today.getDate();
+var cm=today.getMonth()+1;
+var cy=today.getFullYear();
+
+
+var tot=document.getElementById("ans")
+
+
+function result()
+{
+  var d1=Number(bd.value);
+  var m1=Number(bm.value);
+  var y1=Number(by.value);
+  
+  var today=new Date();
+  var d2=today.getDate();
+  var m2=today.getMonth()+1;
+  var y2=today.getFullYear();
+  
+  if (m1>m2){
+    dy=y2-1;
+    dm=m2+12;
+    if (d1>d2){
+      nm=dm-1;
+      nd=d2+30;
+      d=nd-d1;
+      m=nm-m1;
+      y=dy-y1;
+    }
+    else{
+      d=d2-d1;
+      m=dm-m1;
+      y=dy-y1;
+    }
+  }
+  else if (d1>d2){
+    dm=m2-1;
+    dd=d2+30;
+    d=dd-d1;
+    m=dm-m1;
+    y=y2-y1;
+  }
+  else{
+    d=d2-d1;
+    m=m2-m1;
+    y=y2-y1;
+  }
+
+  tot.textContent="You Are Born In "
+  +y+" years "+m+" months "+d+" days";
 }
-if (bm>cm){
-    y = cy-1;
-    m = cm+12;
-    
-}
-
-var d = cd-bd;
-var m = cm-bm;
-var y = cy-by;
-
-document.getElementById("result").innerHTML = 'your age is'+y'year'+m'month'+d'days'
-}
-
